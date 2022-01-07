feb85619e7875f6547b17f78f7cc7f2ce855823d
Billy Yip <yckbilly1929@gmail.com>
2022-01-07 11:33:28 +0800

    fix: package.json types

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -23,3 +23,33 @@ declare module "*.module.styl" {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+// declaration
+/// <reference types="less" />
+import { Plugin } from "esbuild";
+import { Plugin as PostCSSPlugin } from "postcss";
+import { Options as SassOptions } from "sass";
+import stylus from "stylus";
+declare type StylusRenderOptions = Parameters<typeof stylus.render>[1];
+interface PostCSSPluginOptions {
+  plugins: PostCSSPlugin[];
+  modules: boolean | any;
+  rootDir?: string;
+  sassOptions?: SassOptions;
+  lessOptions?: Less.Options;
+  stylusOptions?: StylusRenderOptions;
+  writeToFile?: boolean;
+  enableCache?: boolean;
+}
+export declare const defaultOptions: PostCSSPluginOptions;
+declare const postCSSPlugin: ({
+  plugins,
+  modules,
+  rootDir,
+  sassOptions,
+  lessOptions,
+  stylusOptions,
+  writeToFile,
+  enableCache
+}?: PostCSSPluginOptions) => Plugin;
+export default postCSSPlugin;
